c56e23661ac6bb29c4f6200092642e95a24e54e3
chengensung1219 <chengensung1219@gmail.com>
2024-06-17 13:49:02 -0600

    updated again

--- week08/styles/register.js
+++ week08/styles/register.js
@@ -1,4 +1,4 @@
-import { participantTemplate, successTemplate } from './styles/Templates.js';
+import { participantTemplate, successTemplate } from './Templates.js';
 document.addEventListener('DOMContentLoaded', () => {
     let participantCount = 1;
 
