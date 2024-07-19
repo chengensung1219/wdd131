8ddcfa5852c9302d0ea80d00f8f33bf8f6b23248
chengensung1219 <156931175+chengensung1219@users.noreply.github.com>
2024-07-19 14:18:06 -0600

    minor updated

--- final-project/styles/gc.js
+++ final-project/styles/gc.js
@@ -1,25 +1,7 @@
 
-// Get the elements with class="column"
 var elements = document.getElementsByClassName("column");
 
-// Declare a loop variable
 var i;
-
-// Full-width images
-function one() {
-    for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "100%";  // IE10
-    elements[i].style.flex = "100%";
-    }
-}
-
-// Two images side by side
-function two() {
-    for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "50%";  // IE10
-    elements[i].style.flex = "50%";
-    }
-}
 
 // Four images side by side
 function four() {
@@ -40,10 +22,4 @@ for (var i = 0; i < btns.length; i++) {
     });
 }
 
-const btn1 = document.getElementById("btn1")
-const btn2 = document.getElementById("btn2")
-const btn4 = document.getElementById("btn4")
-
-btn1.addEventListener('click', one)
-btn2.addEventListener('click', two)
-btn4.addEventListener('click', four)
+four();
