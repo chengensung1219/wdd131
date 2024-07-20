c842537ecda601bddda96b0c881ae00dc78f8164
chengensung1219 <156931175+chengensung1219@users.noreply.github.com>
2024-07-20 13:57:47 -0600

    add the placeholder elements

--- final_project/styles/js/Templates.js
+++ final_project/styles/js/Templates.js
@@ -4,29 +4,29 @@ export function activityTemplate(count) {
             <p>Activity ${count}</p>
             <div class="item">
                 <label for="name${count}">Activity<span>*</span></label>
-                <input class="name${count}" type="text" name="name${count}" />
+                <input class="name${count}" type="text" name="name${count}" placeholder="Activity Name"/>
             </div>
 
             <div class="item">
                 <label for="address${count}">Address<span>*</span></label>
-                <input id="address${count}" type="text" name="address${count}" />
+                <input id="address${count}" type="text" name="address${count}" placeholder="Address"/>
             </div>
 
             <div class="item">
                 <label for="city${count}">City<span>*</span></label>
-                <input id="city${count}" type="text" name="city${count}" />
+                <input id="city${count}" type="text" name="city${count}" placeholder="City"/>
             </div>
             <div class="item">
                 <label for="state${count}">State<span>*</span></label>
-                <input id="state${count}" type="text" name="state${count}" />
+                <input id="state${count}" type="text" name="state${count}" placeholder="State"/>
             </div>
             <div class="item">
                 <label for="date${count}">Desired Date <span>*</span></label>
-                <input id="date${count}" type="date" name="date${count}" />
+                <input id="date${count}" type="date" name="date${count}"/>
             </div>
             <div class="item">
                 <label for="message${count}">Message<span>*</span></label>
-                <input id="message${count}" type="text" name="message${count}" />
+                <input id="message${count}" type="text" name="message${count}" placeholder="Another Request/Information"/>
             </div>
             <button type="button" id="add" style="height: 40px; margin-top: 30px">Add Activity</button>
         </section>
